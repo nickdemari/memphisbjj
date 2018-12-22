7b2f75498b062ea8751769ec8411c5a64909ac5d
Nick Demari <icthos@gmail.com>
2018-12-21 19:59:06 -0600

    add instructors screen

---
+++ lib/screens/Instructors/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InstructorsScreen extends StatefulWidget {
+  @override
+  _InstructorsScreenState createState() => _InstructorsScreenState();
+}
+
+class _InstructorsScreenState extends State<InstructorsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
